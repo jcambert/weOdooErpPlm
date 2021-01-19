82c7cf6b619e3bd7a75579b5f104e009d224489c
Ambert Jean-Christophe <jc.ambert@free.fr>
2021-01-19 20:52:37 +0100

    update

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@ from . import plm_type
 from . import plm_stage
 from . import plm_approval_template
 
-from . import plm_activity
+# from . import plm_activity
 from . import plm_erp
 # from . import plm_bom_change
 from . import plm_approval
--- models/plm_erp.py
+++ models/plm_erp.py
@@ -4,7 +4,8 @@ from odoo import models, fields, api,_
 class Plm(models.Model):
     _name='mrp.plm'
     _description='Mrp Plm'
-    _inherit=['mrp.plm.activity','mail.thread','mrp.plm.tag']
+    _inherit=['mail.activity.mixin','mail.thread']
+    
     active=fields.Boolean('Active', default=True, help="Si le champ actif est défini sur False, vous pourrez masquer l'ordre de modification technique sans le supprimer.")
     allow_apply_change=fields.Boolean("Afficher les modifications appliquées",compute='_compute_allow_apply_change')
     # allow_change_stage=fields.Boolean(readonly=True, string="Autoriser le changement d'étape")
@@ -12,7 +13,7 @@ class Plm(models.Model):
     # bom_change_ids=fields.One2many('mrp.plm.bom.change','eco_id',readonly=True,help="Modifications de la nomenclature OMT")
     # bom_id=fields.Many2one('mrp.bom',ondelete='set null',help="Nomenclature")
     # bom_rebase_ids=fields.One2many('mrp.plm.bom.change','rebase_id',help="Refonte de la nomenclature")
-    # color = fields.Integer('Couleur')
+    color = fields.Integer('Couleur')
     # company_id=fields.Many2one('res.company','Company',ondelete='set null',help="Societé")
     # current_bom_id=fields.Many2one('mrp.bom','Current Bom',ondelete='set null',help="Nouvelle nomenclature")
     # display_name=fields.Char(readonly=True,string="Nom affiché")
@@ -37,19 +38,20 @@ class Plm(models.Model):
     # message_unread_counter=fields.Integer(readonly=True,string="Compteur de messages non lus")
     # mrp_document_count=fields.Integer(readonly=True,string="Nb. pièces jointes")
     # mrp_document_ids=fields.One2many('mrp.document','res_id',help="Pièces jointes")
-    # name=fields.Char(required=True,string="Référence")
+    name=fields.Char(required=True,string="Référence")
     # new_bom_id=fields.Many2one('mrp.bom',ondelete='set null',help="Nouvelles nomenclatures")
     # new_bom_revision=fields.Integer(string="Révision de la nomenclature")
-    # note=fields.Text(string='Internal Notes')
+    note=fields.Text(string='Internal Notes')
     # previous_change_ids=fields.One2many('mrp.plm.bom.change','eco_rebase_id',readonly=True, help="Anciennes modifications de l'OMT")
     # priority = fields.Selection([('0', 'Normal'),('1', 'Important'),], default='0', index=True, help="Priority")
     product_tmpl_id = fields.Many2one('product.template', 'Product Template',auto_join=True, ondelete="set null")
     # routing_change_ids=fields.One2many('mrp.plm.routing.change','eco_id',readonly=True,help="Modifications de l'acheminement OMT")
     stage_id=fields.Many2one('mrp.plm.stage',ondelete='restrict',help="Etape")
     state=fields.Selection([('confirmed','Confirmed'),('progress','Progress'),('rebase','Rebase'),('conflict','Conflict'),('done','Done')],'Status',required=True,readonly=True,help="Statut")
-    tag_ids=fields.Many2many('mrp.plm.tag','Tag',string="Étiquettes")
+    # tag_ids=fields.Many2many('mrp.plm.tag','Tag',string="Étiquettes")
+    tag_ids = fields.Many2many('mrp.plm.tag', 'mrp_plm_tags_rel', 'plm_id', 'tag_id', string='Tags')
     # type=fields.Selection([()],'Appliqué sur',required=True,help="Appliqué sur")
-    type_id=fields.Many2one('mrp.plm.type','Type',ondelete='restrict',required=True,help="Type")
+    type_id=fields.Many2one('mrp.plm.type','Type',ondelete='restrict',required=True,help="Type",store=True)
     user_can_approve=fields.Boolean("Peut Approuver",compute='_compute_user_can_approve')
     user_can_reject=fields.Boolean("Peut Refuser",compute='_compute_user_can_reject')
     user_id=fields.Many2one('res.users','Responsable',ondelete='set null',help="responsable")
@@ -63,13 +65,17 @@ class Plm(models.Model):
     is_purchase=fields.Boolean(store=False)
     
     def _compute_allow_apply_change(self):
-        return True
+        for record in self:
+            record.allow_apply_change=True
     def _compute_user_can_approve(self):
-        return True
+        for record in self:
+            record.user_can_approve=True
     def _compute_user_can_reject(self):
-        return True
+        for record in self:
+            record.user_can_reject=True
     def _compute_kanban_state(self):
-        return 'normal'
+        for record in self:
+            record.kanban_state='normal'
     def _compute_is_sale(self):
         if self.product_tmpl_id.exists():
             return self.product_tmpl_id.sale_ok
@@ -91,6 +97,8 @@ class Plm(models.Model):
         defaults['is_sale']=False
         defaults['is_purchase']=False
         defaults['effectivity']='asap'
+        defaults['user_can_approve']=True
+        defaults['user_can_reject']=True
         return defaults
 
     @api.onchange('product_tmpl_id')
